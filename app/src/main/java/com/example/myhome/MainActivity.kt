16a9cdde5f382010c301fb7e6287787da96a941a
JH <finale.uou2021@gmail.com>
2022-05-27 01:11:46 +0900

    지현 & 민우 합쳐놓음 0527

--- app/src/main/java/com/example/myhome/MainActivity.kt
+++ app/src/main/java/com/example/myhome/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.myhome
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnsetting.setOnClickListener{
+            val intent = Intent(this, SettingActivity::class.java)
+            startActivity(intent)
+        }
+
+
     }
 }
